c5434cc1813d96d67e277f103815f3d3347a9772
LudSkop <luda.skopenko80@gmail.com>
2026-04-19 14:07:17 +0300

    crate NotesClient

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,69 +1,22 @@
-"use client";
-
-import css from "@/app/notes/page.module.css";
-import { NoteList } from "@/components/NoteList/NoteList";
-
-import { keepPreviousData } from "@tanstack/react-query";
-import Pagination from "@/components/Pagination/Pagination";
-import { useState } from "react";
-import Modal from "@/components/Modal/Modal";
-import NoteForm from "@/components/NoteForm/NoteForm";
-import { useDebouncedCallback } from "use-debounce";
-import SearchBox from "@/components/SearchBox/SearchBox";
-import { useQuery } from "@tanstack/react-query";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
+import NotesClient from "./Notes.client";
 
-export default function App() {
-  const [page, setPage] = useState(1);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export default async function NotesPage() {
+  const queryClient = new QueryClient();
 
-  const [inputValue, setInputValue] = useState("");
-  const [search, setSearch] = useState("");
-
-  const debouncedSearch = useDebouncedCallback((value: string) => {
-    setSearch(value);
-    setPage(1); // ✅ скидає сторінку при новому пошуку
-  }, 500);
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["notes", page, search], // ключ з номером сторінки
-    queryFn: () => fetchNotes({ page, search }), // передаємо номер сторінки
-    placeholderData: keepPreviousData,
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", 1, ""],
+    queryFn: () => fetchNotes({ page: 1, search: "" }),
   });
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <p>Error</p>;
-  const notes = data?.notes ?? [];
 
   return (
-    <>
-      <div className={css.app}>
-        <header className={css.toolbar}>
-          <SearchBox
-            value={inputValue}
-            onChange={(value) => {
-              setInputValue(value);
-              debouncedSearch(value);
-            }}
-          />
-          {data && data.totalPages > 1 && (
-            <Pagination
-              totalPages={data.totalPages}
-              currentPage={page}
-              onPageChange={setPage}
-            />
-          )}
-          <button className={css.button} onClick={() => setIsModalOpen(true)}>
-            Create note +
-          </button>
-          {isModalOpen && (
-            <Modal onClose={() => setIsModalOpen(false)}>
-              <NoteForm onClose={() => setIsModalOpen(false)} />
-            </Modal>
-          )}
-        </header>
-
-        {notes.length > 0 && <NoteList notes={notes} />}
-      </div>
-    </>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
   );
 }
